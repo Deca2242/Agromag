40b8dc1f51e0ed374f0b7b450830c798f5a1cfde
Cristian <cmbarrera2242@gmail.com>
2026-05-01 18:31:05 -0500

    Initialize Agromag project

---
+++ src/main/java/com/agromag/AgromagApplication.java
@@ -0,0 +1,13 @@
+package com.agromag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgromagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgromagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agromag/AgromagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agromag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgromagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
